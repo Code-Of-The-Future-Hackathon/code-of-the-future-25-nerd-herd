4eb2614e5bdeee22fc960f375ae2a54a9490973c
SkyLestor <timur_skocylov@outlook.com>
2025-02-22 17:30:34 +0200

    Encrupted qr-code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './components/QrCodeGenerator/QrCodeGenerator';
+import QrCodeGenerator from './components/QrCodeGenerator/QrCodeGenerator';
 
 function App() {
+  const [formData, setFormData] = useState({
+    mouse: 28,
+    Paper: 5,
+  });
+  const user = 'User123';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <QrCodeGenerator user={user} formData={formData} />
     </div>
   );
 }
-
 export default App;
